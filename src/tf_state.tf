4d21293cd9c678dd023bb2aa54f2358460ee874b
Petrik van der Velde <petrikvandervelde@gmail.com>
2021-05-30 15:38:45 +1200

    Configure the plugins

--- src/tf_state.tf
+++ src/tf_state.tf
@@ -1,4 +1,16 @@
 terraform {
     backend "local" {
     }
+
+    required_providers {
+        hyperv = {
+            source = "taliesins/hyperv"
+            version = "~> 1.0"
+        }
+
+        windns = {
+            source = "terraform.example.com/portsofportland/windns"
+            version = "~> 0.5"
+        }
+    }
 }
