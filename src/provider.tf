43af4ed9ffb11e97e01975cdfecc688f89d0c284
Petrik van der Velde <petrikvandervelde@gmail.com>
2021-06-15 18:07:36 +1200

    Make the deploy work
    On an AD domain you need an HTTPS connection otherwise the winRM connection will fail with a weird error (401 - invalid content)

--- src/provider.tf
+++ src/provider.tf
@@ -3,14 +3,10 @@ provider "hyperv" {
   password        = var.hyperv_administrator_password
   host            = var.hyperv_server_address
   port            = 5986
-  https           = false
+  https           = true
   insecure        = true
-  use_ntlm        = true
-  tls_server_name = ""
-  cacert_path     = ""
-  cert_path       = ""
-  key_path        = ""
-  script_path     = ""
+  use_ntlm        = false
+  script_path     = "C:/Temp/terraform_%RAND%.cmd"
   timeout         = "30s"
 }
 
