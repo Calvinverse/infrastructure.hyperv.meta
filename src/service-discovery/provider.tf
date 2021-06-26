b4546bb9ff51a47943aaae6e37eb49f65dbf2aa9
Petrik van der Velde <petrikvandervelde@gmail.com>
2021-06-26 17:55:45 +1200

    Increase time-out for the hyper-v provider

--- src/service-discovery/provider.tf
+++ src/service-discovery/provider.tf
@@ -17,15 +17,7 @@ terraform {
 }
 
 provider "hyperv" {
-  user            = var.hyperv_administrator_user
-  password        = var.hyperv_administrator_password
-  host            = var.hyperv_server_address
-  port            = 5986
-  https           = true
-  insecure        = true
-  use_ntlm        = false
-  script_path     = "C:/Temp/terraform_%RAND%.cmd"
-  timeout         = "30s"
+  timeout     = "120s"
 }
 
 provider "windns" {
